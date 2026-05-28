93d2ff1a0b396ff60bff92859bd26599d0fbbd7f
MSC <mikikanet@gmail.com>
2026-05-28 12:41:27 +0200

    Menu ui per rascar diferents tickets

--- src/server/TicketSystem.server.lua
+++ src/server/TicketSystem.server.lua
@@ -1,45 +1,12 @@
 local interactPart = workspace:WaitForChild("TicketTable_Interact")
 local prompt = interactPart:WaitForChild("ProximityPrompt")
 
-local showMessage = game.ReplicatedStorage:WaitForChild("ShowMessage")
+local replicatedStorage = game:GetService("ReplicatedStorage")
+
+local openScratch = replicatedStorage:WaitForChild("OpenScratch")
 
 prompt.Triggered:Connect(function(player)
 
-	local leaderstats = player:WaitForChild("leaderstats")
-
-	local tickets = leaderstats:WaitForChild("BasicTickets")
-	local money = leaderstats:WaitForChild("Money")
-
-	if tickets.Value > 0 then
-
-		tickets.Value -= 1
-
-		local random = math.random(1,100)
-
-		if random <= 60 then
-
-			showMessage:FireClient(player, "Has perdido")
-
-		elseif random <= 95 then
-
-			local reward = 25
-			money.Value += reward
-
-			showMessage:FireClient(player, "+" .. reward .. "$")
-
-		else
-
-			local jackpot = 100
-			money.Value += jackpot
-
-			showMessage:FireClient(player, "JACKPOT! +" .. jackpot .. "$")
-
-		end
-
-	else
-
-		showMessage:FireClient(player, "No tienes tickets básicos")
-
-	end
+	openScratch:FireClient(player)
 
 end)
---
+++ src/shared/TicketData.lua
@@ -0,0 +1,44 @@
+local TicketData = {
+
+	Basic = {
+		Price = 10,
+
+		LoseChance = 50,
+		WinChance = 35,
+		JackpotChance = 5,
+
+		MinReward = 15,
+		MaxReward = 30,
+
+		JackpotReward = 100
+	},
+
+	Silver = {
+		Price = 50,
+
+		LoseChance = 50,
+		WinChance = 40,
+		JackpotChance = 10,
+
+		MinReward = 75,
+		MaxReward = 120,
+
+		JackpotReward = 500
+	},
+
+	Gold = {
+		Price = 150,
+
+		LoseChance = 40,
+		WinChance = 45,
+		JackpotChance = 15,
+
+		MinReward = 200,
+		MaxReward = 400,
+
+		JackpotReward = 2000
+	}
+
+}
+
+return TicketData
